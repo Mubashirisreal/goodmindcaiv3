6507feaf34a881898078d8c7fadc4ec08f6e6b6d
Mubashirisreal <147941230+Mubashirisreal@users.noreply.github.com>
2025-05-02 01:39:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="z-10 flex w-full max-w-5xl flex-col items-center justify-center">
         <h1 className="mb-12 text-center text-4xl font-bold">
-          Kim from Goodmind.
+          Tara from Goodmind.
         </h1>
         <Conversation/>
       </div>
